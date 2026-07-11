32b73e16ca637d300a517fa08e7356366b3ce03a
eben-vranken <vranken.eben@outlook.com>
2026-07-11 18:46:04 +0200

    feat: remove custom requirement checker and use argparse required tag

--- snv-detector.py
+++ snv-detector.py
@@ -5,14 +5,10 @@ from src import parser
 def parse_args():
     parser = ArgumentParser()
 
-    parser.add_argument("-b", "--base", help="The file path to the base genome")
-    parser.add_argument("-m", "--mutated", help="The file path to the mutated genome")
+    parser.add_argument("-b", "--base", required=True, help="The file path to the base genome")
+    parser.add_argument("-m", "--mutated", required=True, help="The file path to the mutated genome")
 
     args = parser.parse_args()
-
-    if not args.base and not args.mutated:
-        print("Base and mutated genomes are required")
-        exit(1)
     
     return args
 
